00294c26502ea26de6de3507143607f9e3b8dda6
Ankit Singh <Ankit.Singh@csgi.com>
2022-08-07 11:34:41 +0530

    prod of digit recursion

--- recursion/sum-of-digits/Program.cs
+++ recursion/sum-of-digits/Program.cs
@@ -6,17 +6,38 @@ namespace sum_of_digits
     {
         public static int SumOfDigits(int num)
         {
+            //Base Condition in the end it will return 0 
+            if (num == 0)
+            {
+                return 0;
+            }
+            //Console.WriteLine(num%10);
+            return (num % 10 + SumOfDigits(num / 10));
+        }
+
+        public static int ProductOfDigits(int num)
+        {
+            // Below base condition is wrong because it will return 0 and anything later multplied by zero will be zero
+            //if (num == 0)
+            //{
+            //    return 0;
+            //}
+
             //Base Condition
-            if(num % 10 == num)
+            //will not return zero, rather simply return 'n' if it is last number which is received in the function input
+            if (num%10 == num)
             {
                 return num;
             }
-            return (num % 10 + SumOfDigits(num / 10));
+            return (num % 10 * ProductOfDigits(num / 10));
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(SumOfDigits(1234));
+            Console.WriteLine(ProductOfDigits(1234));
+            //if number containing zero is given in imput, product should return zero the 
+            Console.WriteLine(ProductOfDigits(12034));
         }
     }
 }
