c66e960104bb3ed085c3cbff29a67c77ad19d434
Ankit Singh <Ankit.Singh@csgi.com>
2022-08-07 20:25:20 +0530

    Queue implementation added

---
+++ binary-tree/preorder/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace preorder
+{
+    internal class Program
+    {
+        public class Node
+        {
+            public int data { get; set; }
+            public Node right { get; set; }
+            public Node left { get; set; }
+
+            // When a new node is created initially there will be no right and left child 
+            public Node(int data)
+            {
+                this.data = data;
+                right = null;
+                left = null;
+            }
+        }
+
+        public static class BinaryTree
+        {
+            // will need a variable to keep count of index where our new node should be inserted.
+            static int idx = -1;
+
+            //This function will build tree using preorder technoque and in the end it will return the result as a head node.
+            public static Node BuildTree(int[] arr)
+            {
+                //keep post incrementing the index count 'idx' by +1. Also, peek elements from the array one by one and insert it into tree.
+                idx++;
+
+                //-1 at any index in array represents null node or leaf node. So,now execution will move back to immediate parent node
+                if (idx < arr.Length && arr[idx] == -1)
+                {
+                    return null;
+                }
+
+                //Create new node
+                Node node = new Node(arr[idx]);
+
+                //*********** PREORDER insertion *********
+
+                //Now recursively call above function Buildtree() --> LEFT CHILD
+                node.left = BuildTree(arr);
+                //Now recursively call above function Buildtree() --> RIGHT CHILD
+                node.right = BuildTree(arr);
+
+                return Object.ReferenceEquals(node, null) ? null : node;
+            }
+
+            // Tree traversal
+            public static void PreOrderTraversal(Node root)
+            {
+                //CORNER CASE : when a node is null, simply return 
+                if (root == null) { return; }
+
+                //Let's print nodes
+                Console.WriteLine(root.data + "");
+
+                //Left subtree
+                PreOrderTraversal(root.left);
+                //Right subtree
+                PreOrderTraversal(root.right);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Tree Insertion!");
+
+            //int[] arr = { 1, 2, -1, 4, -1, 6 };
+            int[] arr = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+            Node root = BinaryTree.BuildTree(arr);
+            Console.WriteLine(root.data);
+            Console.WriteLine("Let's Traverser Now");
+            BinaryTree.PreOrderTraversal(root);
+
+        }
+    }
+}
---
+++ queue/queue-linked-list/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace queue_linked_list
+{
+    internal class Program
+    {
+        public class Node
+        {
+            public string data { get; set; }
+            public Node next { get; set; }
+
+            public Node(string data)
+            {
+                this.data = data;
+                next = null;
+            }
+        }
+        public static class Queue
+        {
+            // creating two node
+            //1. Front node
+            static Node head = null;
+            //2. Rear node
+            static Node tail = null;
+
+            public static Boolean IsEmpty()
+            {
+                return head == null & tail == null;
+            }
+
+            // No need to implement this we can assume that we have enough heap memory
+            // public static Boolean isFull() { }
+
+            public static void Enqueue(string data)
+            {
+                // INSERTION IS ALWAYS DONE ON TAIL or REAR side
+                Node newNode = new Node(data);
+                
+                if (tail == null)
+                {
+                    // when tail is null which means there were no nodes previously, will add our first node.
+                    // for first and only node, head and tail will point to this new node
+                    head = tail = newNode;
+                    return;
+                }
+
+                // else if, some nodes are already present. Then point existing tail node's next to new node from null and
+                // newNode becomes our new tail node
+                tail.next = newNode;
+                tail = newNode;
+            }
+
+            public static string Dequeue()
+            {
+                // DELETION IS ALWAYS DONE ON FRONT side
+
+                // first check if queue is empty, if it is, then there is nothing to delete.
+                if (IsEmpty())
+                {
+                    return "-1";
+                }
+
+                // else, delete front element and return it's value
+                var deletedData = head.data;
+
+                //CHECK!! incase if head and and tall are at same node, i.e when only one element. Tail should be set to null
+                if(head == tail)
+                {
+                    tail = null;
+                } 
+                head = head.next;
+                return deletedData;
+            }
+
+            public static string peek()
+            {
+                if (IsEmpty())
+                {
+                    return "-1";
+                }
+                return tail.data;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Queue.Enqueue("a");
+            Queue.Enqueue("b");
+            Queue.Enqueue("c");
+            Queue.Enqueue("d");
+            Console.WriteLine("Peek: "+ Queue.peek());
+            Console.WriteLine("Deleting: " + Queue.Dequeue());
+            Console.WriteLine("Deleting: " + Queue.Dequeue());
+            Console.WriteLine("Deleting: " + Queue.Dequeue());
+            Console.WriteLine("Deleting: " + Queue.Dequeue());
+            Console.WriteLine("Peek: " + Queue.peek());
+        }
+    }
+}
