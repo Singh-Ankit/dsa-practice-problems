b67840af290edf34b92b46502abcd262b22afc8d
Ankit Singh <Ankit.Singh@csgi.com>
2022-08-21 14:04:43 +0530

    string based easy problems

---
+++ string/defanged-ip-address/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace defanged_ip_address
+{
+    public class Program
+    {
+        public static string DefangedIP(string input) 
+        {
+            string result = "";
+            
+            for(int i = 0; i < input.Length; i++)
+            {
+                //Console.WriteLine(input[i].GetType());
+                if (input[i] == '.')
+                {
+                    //result is of type string, anything can be added to string. It can be char or string itself
+                    result += "[.]";
+                }else
+                {
+                    result += input[i]+""; //or result += string(input[i])
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string str = "255.100.50.0";
+            Console.WriteLine(DefangedIP(str));
+        }
+    }
+}
